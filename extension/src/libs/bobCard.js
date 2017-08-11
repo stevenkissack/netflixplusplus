a231b3d82dc56b893836d74c6113c41032dd8a14
Steven Kissack <stevokk@hotmail.com>
2017-08-11 22:40:12 +1000

    cleaner and more rebust adding to bob cards

--- extension/src/libs/bobCard.js
+++ extension/src/libs/bobCard.js
@@ -2,6 +2,29 @@ import * as icons from './icons';
 import { h } from 'dom-chef'; // JSX vDOM
 import { getRaterUtils } from './utils';
 
+// Specific elements for bob-card elements, both normal and tall
+function generateRatingsElement(details) {
+	return details.Ratings.map(rating => {
+		let parsedRating = getRaterUtils(rating, details);
+		
+		// Don't know this rater
+		if(!parsedRating) return;
+
+		return (
+			<div class="nf-svg-button-wrapper">
+				{ parsedRating.link ? (
+					<a href={parsedRating.link} target="_blank" class={"nf-svg-button simpleround npp-icon npp-icon-" + parsedRating.id }>
+						<span>{ parsedRating.score }</span>
+					</a>
+				) : (
+					<a class={"nf-svg-button simpleround npp-icon-" + parsedRating.id }>
+						<span>{ parsedRating.score }</span>
+					</a>
+				)}
+			</div>
+		); 
+	});
+}
 
 export function enhanceBobCard(details, node) {
 	let enhanceDom;
@@ -12,26 +35,7 @@ export function enhanceBobCard(details, node) {
 		//.Error Will contain the phrase "not found"
 		return; // TODO: Show error box?
 	} else {
-		enhanceDom = details.Ratings.map(rating => {
-			let parsedRating = getRaterUtils(rating, details);
-			
-			// Don't know this rater
-			if(!parsedRating) return;
-
-			return (
-				<div class="nf-svg-button-wrapper">
-					{ parsedRating.link ? (
-						<a href={parsedRating.link} target="_blank" class={"nf-svg-button simpleround npp-icon-" + parsedRating.id }>
-							<span>{ parsedRating.score }</span>
-						</a>
-					) : (
-						<a class={"nf-svg-button simpleround npp-icon-" + parsedRating.id }>
-							<span>{ parsedRating.score }</span>
-						</a>
-					)}
-				</div>
-			); 
-		})
+		enhanceDom = generateRatingsElement(details);
 	}
 	/*
 	  let newNode = document.createElement('span')
@@ -39,8 +43,20 @@ export function enhanceBobCard(details, node) {
 		newNode.style = ''
 	 */
 	let buttonWrapper = node.querySelector('.bob-last-content .bob-button-wrapper');
-	if(buttonWrapper && buttonWrapper.firstChild) {
-		buttonWrapper.insertBefore(<div>{ enhanceDom }</div>, buttonWrapper.firstChild);
+	let isTall = node.className.indexOf('bob-card-tall-panel') !== -1;
+
+	let wrappedRatings = <div class="npp-wrapper">{ enhanceDom }</div>;
+	if(buttonWrapper && !isTall) {
+		if(buttonWrapper.firstChild) {
+			buttonWrapper.insertBefore(wrappedRatings, buttonWrapper.firstChild);
+		} else {
+			buttonWrapper.appendChild(wrappedRatings);
+		}
+		buttonWrapper.classList.add('npp-nudge-down');
+	} else {
+		// Failed to find the best place on the card or is the tall design,
+		// so attach to just the card and position absolute
+		node.appendChild(wrappedRatings);
 	}
 };
 
